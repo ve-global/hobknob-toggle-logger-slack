65a12de674a593580a99d476073f8715fb8c1a40
Andy Royle <ajroyle@gmail.com>
2016-07-19 12:08:53 +0100

    fix case where toggleName is null (for simple toggles)

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ var getUsername = (user) => {
     : user.name.givenName + ' ' + user.name.familyName;
 };
 
+var formatToggleName = (featureName, toggleName) => {
+  return !!toggleName
+  ? `${featureName}/${toggleName}`
+  : `${featureName}`;
+};
+
 module.exports = {
   /*
   {
@@ -50,8 +56,9 @@ module.exports = {
   }
   */
   addFeatureToggle: function(toggleEvent, next) {
+    var t = formatToggleName(toggleEvent.featureName, toggleEvent.toggleName);
     slack.send({
-        text: `${getUsername(toggleEvent.user)} added ${toggleEvent.featureName}/${toggleEvent.toggleName} to ${toggleEvent.applicationName}`,
+        text: `${getUsername(toggleEvent.user)} added ${t} to ${toggleEvent.applicationName}`,
         channel: '#deployments',
         username: 'hobknob'
     });
@@ -67,8 +74,9 @@ module.exports = {
   }
   */
   updateFeatureToggle: function(updateEvent, next) {
+    var t = formatToggleName(updateEvent.featureName, updateEvent.toggleName);
     slack.send({
-        text: `${getUsername(updateEvent.user)} changed ${updateEvent.applicationName} ${updateEvent.featureName}/${updateEvent.toggleName} to ${updateEvent.value}`,
+        text: `${getUsername(updateEvent.user)} changed ${updateEvent.applicationName} ${t} to ${updateEvent.value}`,
         channel: '#deployments',
         username: 'hobknob'
     });
@@ -83,8 +91,9 @@ module.exports = {
   }
   */
   deleteFeatureToggle: function(deleteEvent, next) {
+    var t = formatToggleName(deleteEvent.featureName, deleteEvent.toggleName);
     slack.send({
-        text: `${getUsername(deleteEvent.user)} deleted ${deleteEvent.featureName}/${deleteEvent.toggleName} from ${deleteEvent.applicationName}`,
+        text: `${getUsername(deleteEvent.user)} deleted ${t} from ${deleteEvent.applicationName}`,
         channel: '#deployments',
         username: 'hobknob'
     });
